abbda50cada3909bae5b158228102d13f4d1be2c
Donbazile07 <donbazile007@gmail.com>
2022-08-02 09:39:20 -0400

    Update

---
+++ functionPractice.js
@@ -0,0 +1,20 @@
+for (i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+function cube(num){
+    res = num * num * num
+}
+
+cube(3)
+console.log(cube(3))
+
+console.log('test')
+
+
+function addNumber(a, b){
+    return a + b
+}
+console.log(addNumber(4,8))
+console.log(addNumber(4,8))
+console.log(addNumber(4,8))
